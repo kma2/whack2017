4e3810d5ed11d2729ef2a99d93c89093979c8df8
Katy <kma2@wellesley.edu>
2017-01-09 13:27:52 -0500

    faster animations

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   // fade in cover elements
   $('.cover__container h5').hide().delay(200).fadeIn('slow');
-  $('.cover__headline').hide().fadeIn(3000);
-  $('.cover__intro').hide().delay(1000).fadeIn('slow');
-  $('.cover__container a').hide().delay(1000).fadeIn('slow');
+  $('.cover__headline').hide().fadeIn(2000);
+  $('.cover__intro').hide().delay(500).fadeIn('slow');
+  $('.cover__container a').hide().delay(500).fadeIn('slow');
 
   $(window).scroll( function(){
     $('.hideme').each( function(i){
@@ -11,7 +11,7 @@ $(document).ready(function() {
       var bottom_of_window = $(window).scrollTop() + $(window).height();
 
       if( bottom_of_window > bottom_of_object - 250){
-          $(this).animate({'opacity':'1'}, 1000);
+          $(this).animate({'opacity':'1'}, 800);
       }
     });
   });
